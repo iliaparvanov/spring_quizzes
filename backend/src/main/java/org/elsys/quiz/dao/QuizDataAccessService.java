aae5679c14e8a735f97f7d892549d952c41434bd
Ilia <ilia.nikolaev.parvanov@gmail.com>
2020-01-05 01:25:13 -0800

    Fixes to schema and added example quiz

--- backend/src/main/java/org/elsys/quiz/dao/QuizDataAccessService.java
+++ backend/src/main/java/org/elsys/quiz/dao/QuizDataAccessService.java
@@ -1,8 +1,32 @@
 package org.elsys.quiz.dao;
 
+import org.elsys.quiz.models.Quiz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-@Repository("h2")
-public class QuizDataAccessService {
+import java.util.List;
+import java.util.Optional;
 
+@Repository("h2")
+public class QuizDataAccessService implements QuizDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public QuizDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public Optional<Quiz> getQuizById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Quiz> getAllQuizzes() {
+        return jdbcTemplate.query("SELECT * FROM Quizzes", () ->{
+
+        });
+    }
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -11,6 +11,8 @@ CREATE TABLE Quizzes (
 CREATE TABLE Questions (
     Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Text VARCHAR(100) NOT NULL,
+    QuizId INT NOT NULL,
+
     FOREIGN KEY (QuizId) REFERENCES Quizzes(Id)
 );
 
@@ -18,11 +20,29 @@ CREATE TABLE Answers (
     Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Text VARCHAR(100) NOT NULL,
     IsCorrect BOOLEAN NOT NULL,
-    FOREIGN KEY (QuizId) REFERENCES Quizzes(Id)
+    QuestionId INT NOT NULL,
+
+    FOREIGN KEY (QuestionId) REFERENCES Questions(Id)
 );
 
 CREATE TABLE Submissions (
     Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Score INT NOT NULL,
+    QuizId INT NOT NULL,
+
     FOREIGN KEY (QuizId) REFERENCES Quizzes(Id)
 );
+
+INSERT INTO Quizzes VALUES (1, 'Biology');
+
+INSERT INTO Questions VALUES (1, 'Which famous scientist introduced the idea of natural selection?', 1);
+INSERT INTO Answers VALUES (1, 'Charles Darwin', TRUE, 1);
+INSERT INTO Answers VALUES (2, 'Pesho Peshev', FALSE, 1);
+INSERT INTO Answers VALUES (3, 'Isaac Newton', FALSE, 1);
+INSERT INTO Answers VALUES (4, 'Louis Pasteur', FALSE, 1);
+
+INSERT INTO Questions VALUES (2, 'What are biologists interested in?', 1);
+INSERT INTO Answers VALUES (5, 'flora', TRUE, 2);
+INSERT INTO Answers VALUES (6, 'music', FALSE, 2);
+INSERT INTO Answers VALUES (7, 'fauna', TRUE, 2);
+INSERT INTO Answers VALUES (8, 'literature', FALSE, 2);
